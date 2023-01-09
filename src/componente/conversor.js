ee29b835309698863f6850a41376a06246a524b2
SamuelManhique <100516269+SamuelManhique@users.noreply.github.com>
2023-01-09 06:09:45 -0800

    primeiro commit

---
+++ src/componente/conversor.js
@@ -0,0 +1,12 @@
+import React, { component } from 'react';
+
+export default class Conversor extends component {
+    render() {
+        return
+        {
+            <div>
+
+            </div>
+        }
+    }
+}
